5a52a0bd97bffa3963267058de5cadf033038511
Marcelle Guine <marcelleguine@Macbook-da-Marcelle.local>
2013-02-07 15:51:25 -0200

    Modifica RunMac.sh

--- projeto/War Game of Thrones/baselines/0.6_29-01-2013/RunMac.sh
+++ projeto/War Game of Thrones/baselines/0.6_29-01-2013/RunMac.sh
@@ -1,3 +1,3 @@
 @echo off
 echo Release Launcher
-java -Djava.library.path=../lwjgl-2.8.5/native/macosx -jar War_Game_of_Thrones.jar
+/Library/Java/JavaVirtualMachines/1.6.0_29-b11-402.jdk/Contents/Home/bin/java -Djava.library.path=../lwjgl-2.8.5/native/macosx -jar War_Game_of_Thrones.jar
