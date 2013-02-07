224f3e30e7c65e199a88d55d17ca8da945ca9918
Rodrigo NTI <castro.digao@gmail.com>
2013-02-07 12:10:07 -0200

    Fazendo troca de cartas nível 1 e 2 (mesma coisa) e criando esqueleto dos testes de troca de cartas nível 1

--- projeto/War Game of Thrones/src/models/Difficulty.java
+++ projeto/War Game of Thrones/src/models/Difficulty.java
@@ -1,6 +1,10 @@
 package models;
 
+import java.util.List;
+
 abstract class Difficulty {
+
+    public AIPlayer player;
 
     /**
      * Faz a IA distribuir os exércitos pendentes para os territórios que ela
@@ -13,6 +17,12 @@ abstract class Difficulty {
      * rodada de ataque e passar para a de movimentação.
      */
     abstract protected boolean keepAttacking();
+
+    /**
+     * Pergunta à IA se ela quer trocar suas cartas. Se ela quiser, retornará um array de 3 cartas,
+     * senão, retornará nulo.
+     */
+    abstract public List<CardTerritory> tradeCards();
 
     /**
      * Pergunta à IA qual o próximo ataque dela. A classe AttackData contém o
@@ -44,4 +54,31 @@ abstract class Difficulty {
      * @return
      */
     abstract public TerritoryTransaction nextMove();
+
+    /**
+     * Retorna o tipo de carta selecionada para a troca.
+     * Retorna 0 se não for possível uma troca válida, de 1 a 4 para os tipos padrões de cartas, e 5 para trocar uma
+     * carta de cada tipo.
+     */
+    protected int getCardSelectedType(int triangleCards, int circleCards, int squareCards, int jokerCards) {
+        int selectedType = 0;
+        if ((triangleCards + jokerCards) >= 3) {
+            selectedType = CardTerritory.TRIANGLE;
+        } else if ((circleCards + jokerCards) >= 3) {
+            selectedType = CardTerritory.CIRCLE;
+        } else if ((squareCards + jokerCards) >= 3) {
+            selectedType = CardTerritory.SQUARE;
+        } else if (jokerCards >= 3) {
+            selectedType = CardTerritory.JOKER;
+        } else {
+            triangleCards = triangleCards >= 1 ? 1 : 0;
+            circleCards = circleCards >= 1 ? 1 : 0;
+            squareCards = squareCards >= 1 ? 1 : 0;
+            jokerCards = jokerCards >= 1 ? 1 : 0;
+            if ((triangleCards + circleCards + squareCards + jokerCards) >= 3) {
+                selectedType = 5; // Indica que vou trocar uma de cada tipo
+            }
+        }
+        return selectedType;
+    }
 }
---
+++ projeto/War Game of Thrones/test/models/EasyAITest.java
@@ -0,0 +1,67 @@
+package models;
+
+import java.util.ArrayList;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+import static org.mockito.Mockito.*;
+
+/**
+ * @author rodcastro
+ *
+ */
+public class EasyAITest {
+
+    Player player;
+    EasyAI ai;
+
+    @BeforeClass
+    public void setUp() {
+    }
+
+    @Test
+    public void testTrocaDeCartasComPlayerSemCartasSuficientes() {
+        
+    }
+
+    @Test
+    public void testTrocaDeCartasComPlayerSemCartasDosTiposCertos() {
+
+    }
+
+    @Test
+    public void testTrocaDeCartasComCartasCircle() {
+
+    }
+
+    @Test
+    public void testTrocaDeCartasComCartasTriangle() {
+
+    }
+
+    @Test
+    public void testTrocaDeCartasComCartasSquare() {
+
+    }
+
+    @Test
+    public void testTrocaDeCartasComCartasJoker() {
+
+    }
+
+    @Test
+    public void testTrocaDeCartasComCartasDeCadaTipoNormal() {
+
+    }
+
+    @Test
+    public void testTrocaDeCartasComCartasDeCadaTipoComJoker() {
+
+    }
+
+    @AfterClass
+    public void cleanUp() {
+        // code that will be invoked after this test ends
+    }
+}
